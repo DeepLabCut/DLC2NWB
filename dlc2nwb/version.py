3ec55c673bf313656f06127ac31d5d9159e3333a
AlexEMG <alexander@deeplabcut.org>
2023-01-26 19:12:15 +0100

    version update, solves #17

--- dlc2nwb/version.py
+++ dlc2nwb/version.py
@@ -1,2 +1,2 @@
-__version__ = "0.1"
+__version__ = "0.3"
 VERSION = __version__
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dlc2nwb",
-    version="0.1",
+    version="0.3",
     author="A. & M. Mathis Labs",
     author_email="alexander@deeplabcut.org",
     description="DeepLabCut <-> NWB conversion utilities",
